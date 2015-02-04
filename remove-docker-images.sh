3bed587251bed3135773636293d3500ede09b98e
Giorgos Logiotatidis <seadog@sealabs.net>
2015-02-04 10:58:55 +0200

    Add docker maintenance script.

---
+++ remove-docker-images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Clean up docker reserved space due to bug https://github.com/docker/docker/issues/3182
+
+set -xe
+echo 'Removes all docker images'
+docker rm `docker ps -a -q`
+service docker stop
+rm -rf /var/lib/docker/devicemapper
+service docker start
