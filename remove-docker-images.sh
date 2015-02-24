b2d1468cfac3fdaa6c89ceaf2509723d3ae10ec9
Giorgos Logiotatidis <seadog@sealabs.net>
2015-02-24 13:16:06 +0200

    Update remove-docker-images.

--- remove-docker-images.sh
+++ remove-docker-images.sh
@@ -2,9 +2,12 @@
 # Clean up docker reserved space due to bug https://github.com/docker/docker/issues/3182
 
 set -xe
-echo 'Removes all docker images'
-docker rm `docker ps -a -q`
+
+# Delete all containers
+docker rm $(docker ps -a -q)
+# Delete all images
+docker rmi $(docker images -q)
+
 service docker stop
-rm -rf /var/lib/docker/devicemapper
 service docker start
 service jenkins restart
